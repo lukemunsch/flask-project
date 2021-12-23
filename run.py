8fba3151b90d7d22e80f906e3d4dc9d0a5533a8f
lukemunsch <lpmunsch@gmail.com>
2021-12-23 10:18:40 +0000

    add content changes to run.py file

--- run.py
+++ run.py
@@ -1,19 +1,17 @@
 import os
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
 @app.route("/")
-
-
 def index():
-    return "Hello, World!"
+    return "Hello, World"
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True
-    )
+        debug=True)

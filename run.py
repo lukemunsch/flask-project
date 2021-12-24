7b24ec3db56b9476c88ccda9e2077c6ddfabf0d7
lukemunsch <lpmunsch@gmail.com>
2021-12-24 14:02:32 +0000

    add changes to each html doc

--- run.py
+++ run.py
@@ -12,17 +12,17 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About Page")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact Us")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Come and work with us!")
 
 
 if __name__ == "__main__":
